ded1e16c7e7c35f5c7839af4d91206476971d1ce
Mirza <19375412+doneforaiur@users.noreply.github.com>
2021-05-26 18:26:53 +0300

    Update 3rd.py

--- 3rd.py
+++ 3rd.py
@@ -1,43 +1,35 @@
-class Node:
-    def __init__(self):
-        self.left = None
-        self.right = None
+class newNode: 
+	def __init__(self):
+		self.left = None
+		self.right = None
+ 
+# problem; recursion 
+def checkDupUtil(root, nodes) :
+    if (root == None) :
+        return False
+		
+    if root in nodes: 
+        return True
+  
+    nodes.add(root) 
 	
-def inorderTraversal(root):
-	current = root
-	stack = []
-	res = []
-	
-	while True:
-		if current is not None:
-			stack.append(current)
-			current = current.left
-		elif(stack):
-			current = stack.pop()
-			res.append(current)
-			current = current.right
-		else:
-			break
-	
-	return res
-
-
-def check_collision(root):
-	nodes = inorderTraversal(root)
-	check = any(nodes.count(node) > 1 for node in nodes)
-	return check
+    return checkDupUtil(root.left, nodes) or checkDupUtil(root.right, nodes) 
+ 
+def checkDup(root):
+    nodes=set() 
+    return checkDupUtil(root, nodes) 
+  
 
 if __name__ == '__main__':
-	root = Node()
-	
-	faulty_link = Node()
-	
-	root.left  = Node()
-	root.right = Node()
-	
-	root.left.right = faulty_link
-	#root.left.right = Node()
-	root.right.left = faulty_link
-	
+	root = newNode() 
+	root.right = root
 
-	print(check_collision(root))
+	faulty_link = newNode()
+
+	root.left = faulty_link
+	root.left.left = faulty_link 
+	if (checkDup(root)):
+		print("Yes")
+	else:
+		print("No")
+  
