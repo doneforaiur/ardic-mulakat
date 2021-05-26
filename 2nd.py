f80313f728a587ca7fb49a0e91db166db0d51607
Mirza <19375412+doneforaiur@users.noreply.github.com>
2021-05-26 14:57:47 +0300

    Update 2nd.py

--- 2nd.py
+++ 2nd.py
@@ -1,28 +1,23 @@
 # Zeller's congruence
 # https://en.wikipedia.org/wiki/Zeller%27s_congruence
 
-# Accuracy check
-# https://www.timeanddate.com/calendar/weekday-sunday-1
-
 def zeller(day, month, year):
-	if month == 1:
-		month = 13
-		year = year - 1
-	elif month == 2:
-		month = 14
+	if month < 3:
+		month = 12 + month
 		year = year - 1
 	
-	q = day
-	m = month
-	k = year % 100
-	j = year // 100
+	# 0: Saturday, 1: Sunday, 2: Monday, ..., 6: Friday
+	day_of_the_week =( day + 13 * (month + 1) // 5 + (year % 100) + (year % 100) // 4 + (year // 100) // 4 + 5 * (year // 100) ) % 7
 	
-	h = q + 13 * (m + 1) // 5 + k + k // 4 + j // 4 + 5 * j
-	h = h % 7
-	if h == 2:
+	if day_of_the_week == 1:
+		if month > 12:
+			month = month - 12
+			year = year + 1
 		print(str(day) + "/" + str(month) + "/" + str(year))
 	
+	
 if __name__ == '__main__':
-	for year in range(1900, 2000):
+	
+	for year in range(1900, 3000):
 		for month in range(1,12):
 			zeller(1,month,year)
