399907da15f20a4e889751b646220c969b1a9a4d
Mirza <19375412+doneforaiur@users.noreply.github.com>
2021-05-26 19:48:36 +0300

    Update 2nd.py

--- 2nd.py
+++ 2nd.py
@@ -5,23 +5,25 @@ def zeller(day, month, year):
 	if month < 3:
 		year -= 1
 	
-	day_offsets = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4] # [int(2.6*month - 0.2) for month in range(1,12)
+	# remainder of cumulative day difference between
+	# previous months divided by number of weekdays
+	day_offsets = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
 	
 	# leap year offsets
 	lyo_4   = year // 4
 	lyo_100 = year // 100
 	lyo_400 = year // 400
 	
-	prev_month_offsets = month - 1
+	prev_month_offset = month - 1
 	
-	# 0: Sunday, 1: Monday, ..., 6: Saturday.
-	day_of_week = year + lyo_4 - lyo_100 + lyo_400 + day_offsets[prev_month_offsets] + day
+	day_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+	day_of_week = year + lyo_4 - lyo_100 + lyo_400 + day_offsets[prev_month_offset] + day
 	
 	days_in_week = 7
 	
 	day_of_week = day_of_week % days_in_week
 	
-	if day_of_week == 0:
+	if day_names[day_of_week] == "Monday":
 		if month < 3:
 			year += 1
 		print(day,month, year)
