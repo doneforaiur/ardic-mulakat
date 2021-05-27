565880e20f289d73d1da05597479394f9a5b2c94
Mirza <19375412+doneforaiur@users.noreply.github.com>
2021-05-27 07:34:51 +0300

    Update 2nd.py

--- 2nd.py
+++ 2nd.py
@@ -1,7 +1,7 @@
 # Sakamoto's method
 # https://en.wikipedia.org/wiki/Determination_of_the_day_of_the_week#Sakamoto's_methods
 
-def zeller(day, month, year):
+def is_sunday(day, month, year):
 	if month < 3:
 		year -= 1
 	
@@ -23,7 +23,7 @@ def zeller(day, month, year):
 	
 	day_of_week = day_of_week % days_in_week
 	
-	if day_names[day_of_week] == "Monday":
+	if day_names[day_of_week] == "Sunday":
 		if month < 3:
 			year += 1
 		print(day,month, year)
@@ -31,4 +31,4 @@ def zeller(day, month, year):
 if __name__ == '__main__':
 	for year in range(1900, 2000):
 		for month in range(1,12):
-			zeller(1,month,year)
+			is_sunday(1,month,year)
